7397b3c2bf16acf08d7d503fe933d05d907cce10
FrancescoTroccoli <34275037+FrancescoTroccoli@users.noreply.github.com>
2017-12-25 19:03:33 +0100

    Update EmpModel.java

--- src/cn/itcast/invoice/auth/emp/vo/EmpModel.java
+++ src/cn/itcast/invoice/auth/emp/vo/EmpModel.java
@@ -1,191 +1,139 @@
-package cn.itcast.invoice.auth.emp.vo;
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@taglib prefix="s" uri="/struts-tags"%>
+<%@ page import="cn.itcast.invoice.auth.emp.vo.EmpModel" %>
+<link href="css/index.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="js/jquery-1.8.3.js"></script>
+<script type="text/javascript" src="js/Calendar.js"></script>
+<script type="text/javascript">
+	$(function() {
+		$("#all").click(function() {
+			$("[name=roles]:checkbox").attr("checked",$("#all").attr("checked")=="checked");
+		});
+		$("#reverse").click(function() {
+			$("[name=roles]:checkbox").each(function () {
+                $(this).attr("checked", !$(this).attr("checked"));
+            });
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
-import cn.itcast.invoice.auth.dep.vo.DepModel;
-import cn.itcast.invoice.auth.role.vo.RoleModel;
-import cn.itcast.invoice.util.format.FormatUtil;
-
-public class EmpModel {
-	public static final Integer EMP_GENDER_OF_MAN = 1;
-	public static final Integer EMP_GENDER_OF_WOMAN = 0;
-	
-	public static final String EMP_GENDER_OF_MAN_VIEW = "男";
-	public static final String EMP_GENDER_OF_WOMAN_VIEW = "女";
-	
-	public static Map<Integer, String> genderMap = new HashMap<Integer, String>();
-	
-	static{
-		genderMap.put(EMP_GENDER_OF_MAN, EMP_GENDER_OF_MAN_VIEW);
-		genderMap.put(EMP_GENDER_OF_WOMAN, EMP_GENDER_OF_WOMAN_VIEW);
-	}
-	
-	private Long uuid;
-	//登陆用户名
-	private String userName;
-	//密码
-	private String pwd;
-	//真实姓名
-	private String name;
-	
-	private String email;
-	private String tele;
-	private String address;
-	private String lastLoginIp;
-	
-	private Long birthday;
-	private Long lastLoginTime;
-
-	private Integer gender;
-	private Integer loginTimes;
-	
-	//视图值
-	private String birthdayView;
-	private String lastLoginTimeView;
-	private String genderView;
-	
-	//关系
-	private DepModel dm;
-	private Set<RoleModel> roles;
-	
-	//权限校验辅助字段
-	private String resValue;
-	
-	public String getResValue() {
-		return resValue;
-	}
-
-	public void setResValue(String resValue) {
-		this.resValue = resValue;
-	}
-
-	public Set<RoleModel> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(Set<RoleModel> roles) {
-		this.roles = roles;
-	}
-
-	public Long getUuid() {
-		return uuid;
-	}
-
-	public String getBirthdayView() {
-		return birthdayView;
-	}
-	
-	public String getGenderView() {
-		return genderView;
-	}
-
-	public String getLastLoginTimeView() {
-		return lastLoginTimeView;
-	}
-
-	public void setUuid(Long uuid) {
-		this.uuid = uuid;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getPwd() {
-		return pwd;
-	}
-
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getTele() {
-		return tele;
-	}
-
-	public void setTele(String tele) {
-		this.tele = tele;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getLastLoginIp() {
-		return lastLoginIp;
-	}
-
-	public void setLastLoginIp(String lastLoginIp) {
-		this.lastLoginIp = lastLoginIp;
-	}
-
-	public Long getBirthday() {
-		return birthday;
-	}
-
-	public void setBirthday(Long birthday) {
-		this.birthday = birthday;
-		this.birthdayView = FormatUtil.formatDate(birthday);
-	}
-
-	public Long getLastLoginTime() {
-		return lastLoginTime;
-	}
-
-	public void setLastLoginTime(Long lastLoginTime) {
-		this.lastLoginTime = lastLoginTime;
-		this.lastLoginTimeView = FormatUtil.formatDate(lastLoginTime);
-	}
-
-	public Integer getGender() {
-		return gender;
-	}
-
-	public void setGender(Integer gender) {
-		this.gender = gender;
-		this.genderView = genderMap.get(gender);
-	}
-
-	public Integer getLoginTimes() {
-		return loginTimes;
-	}
-
-	public void setLoginTimes(Integer loginTimes) {
-		this.loginTimes = loginTimes;
-	}
-
-	public DepModel getDm() {
-		return dm;
-	}
-
-	public void setDm(DepModel dm) {
-		this.dm = dm;
-	}
-	
-}
+		});
+	});
+</script>
+<div class="content-right">
+	<div class="content-r-pic_w">
+		<div style="margin:8px auto auto 12px;margin-top:6px">
+			<span class="page_title">员工管理</span>
+		</div>
+	</div>
+	<div class="content-text">
+		<div class="square-order">
+			<s:form action="emp_save" method="post"> 
+			<s:hidden name="em.uuid"/>
+  			<div style="border:1px solid #cecece;">
+				<table width="100%"  border="0" cellpadding="0" cellspacing="0">
+				  <tr bgcolor="#FFFFFF">
+				    <td>&nbsp;</td>
+				  </tr>
+				</table>
+				<table width="100%"  border="0" cellpadding="0" cellspacing="0">
+				    <tr  bgcolor="#FFFFFF">
+				      <td width="18%" height="30" align="center">用&nbsp;户&nbsp;名</td>
+				      <td width="32%">
+				      	<s:textfield name="em.userName" size="25"/>
+				      </td>
+				      <td width="18%"align="center">真实姓名</td>
+				      <td width="32%">
+				      	<s:textfield name="em.name" size="25"/>
+					  </td>
+				    </tr>
+				    <tr bgcolor="#FFFFFF">
+					  <td colspan="4">&nbsp;</td>
+					</tr>
+				    <tr  bgcolor="#FFFFFF">
+				      <td align="center">密&nbsp;&nbsp;&nbsp;&nbsp;码</td>
+				      <td>
+				      	<s:textfield name="em.pwd" size="25"/>
+				      </td>
+				      <td  align="center">确认密码</td>
+				      <td >
+				      	<input type="text" size="25"/>
+				      </td>
+				    </tr>
+				     <tr bgcolor="#FFFFFF">
+					  <td colspan="4">&nbsp;</td>
+					</tr>
+				    <tr  bgcolor="#FFFFFF">
+				      <td height="30" align="center">电子邮箱</td>
+				      <td>
+				      	<s:textfield name="em.email" size="25"/>
+				      <td align="center">电话号码</td>
+				      <td>
+				      	<s:textfield name="em.tele" size="25"/>
+					  </td>
+				     </tr>
+				      <tr bgcolor="#FFFFFF">
+					  <td colspan="4">&nbsp;</td>
+					</tr>
+				    <tr  bgcolor="#FFFFFF">
+				      <td height="30" align="center">性&nbsp;&nbsp;&nbsp;&nbsp;别</td>
+				      <td>
+				      	<s:select name="em.gender" list="@cn.itcast.invoice.auth.emp.vo.EmpModel@genderMap" cssStyle="width:190px"></s:select>
+					  </td>
+				      <td align="center">地&nbsp;&nbsp;&nbsp;&nbsp;址</td>
+				      <td>
+				      	<s:textfield name="em.address" size="25"/>
+					  </td>
+				    </tr>
+				     <tr bgcolor="#FFFFFF">
+					  <td colspan="4">&nbsp;</td>
+					</tr>
+				    <tr  bgcolor="#FFFFFF">
+				      <td height="30" align="center">出生日期</td>
+				      <td>
+				      	<input type="text" value="${em.getBirthdayView()}" size="25" onfocus="c.showMoreDay=false;c.show(this);" readonly="true"/>
+				      	<s:hidden name="em.birthday"/>
+					  </td>
+				      <td align="center">所属部门</td>
+				      <td>
+				      	<s:select name="em.dm.uuid" list="depList" listKey="uuid" listValue="name" cssStyle="width:190px"></s:select>
+					  </td>
+				    </tr>
+				    <tr  bgcolor="#FFFFFF">
+				      <td colspan="4">&nbsp;</td>
+				    </tr>
+				    <tr  bgcolor="#FFFFFF">
+				      <td width="18%" height="30" align="center">角色名称</td>
+				      <td width="82%" colspan="3">
+				      	<input type="checkbox" id="all">全选&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				      	<input type="checkbox" id="reverse">反选
+				      </td>
+				    </tr>
+				    <tr  bgcolor="#FFFFFF">
+				      <td width="18%" height="30" align="center">&nbsp;</td>
+				      <td width="82%" colspan="3">
+				      	<s:checkboxlist name="roleUuids" list="roleList" listKey="uuid" listValue="name"></s:checkboxlist>
+				      </td>
+				    </tr>
+				    <tr  bgcolor="#FFFFFF">
+				      <td colspan="4">&nbsp;</td>
+				    </tr>
+				</table>
+			</div>
+			<div class="order-botton">
+				<div style="margin:1px auto auto 1px;">
+					<table width="100%"  border="0" cellpadding="0" cellspacing="0">
+					  <tr>
+					    <td>
+					    	<a href="javascript:document.forms[0].submit()"><img src="images/order_tuo.gif" border="0" /></a>
+					    </td>
+					    <td>&nbsp;</td>
+					    <td><a href="#"><img src="images/order_tuo.gif" border="0" /></a></td>
+					    <td>&nbsp;</td>
+					    <td><a href="#"><img src="images/order_tuo.gif" border="0" /></a></td>
+					  </tr>
+					</table>
+				</div>
+			</div>
+			</s:form>
+		</div><!--"square-order"end-->
+	</div><!--"content-text"end-->
+	<div class="content-bbg"><img src="images/content_bbg.jpg" /></div>
+</div>
