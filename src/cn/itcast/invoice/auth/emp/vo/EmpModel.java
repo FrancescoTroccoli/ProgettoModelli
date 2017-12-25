242f429e7921d09d2671ea3bc81957bafea16189
FrancescoTroccoli <34275037+FrancescoTroccoli@users.noreply.github.com>
2017-12-25 19:03:49 +0100

    Update EmpModel.java

--- src/cn/itcast/invoice/auth/emp/vo/EmpModel.java
+++ src/cn/itcast/invoice/auth/emp/vo/EmpModel.java
@@ -1,6 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@taglib prefix="s" uri="/struts-tags"%>
-<%@ page import="cn.itcast.invoice.auth.emp.vo.EmpModel" %>
 <link href="css/index.css" rel="stylesheet" type="text/css" />
 <script type="text/javascript" src="js/jquery-1.8.3.js"></script>
 <script type="text/javascript" src="js/Calendar.js"></script>
